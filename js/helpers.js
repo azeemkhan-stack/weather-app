77e4c0bf1cf0eb76a69b4d3c1d6e8c071cf682c0
Azeem Khan <azeemkhangithub@gmail.com>
2026-09-23 16:47:04 +0500

    Fix syntax error in wind direction logic

--- js/helpers.js
+++ js/helpers.js
@@ -23,7 +23,7 @@ export function formatDate(dateObj, options = {}) {
 export function getWindDirection(degrees) {
   const directions = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW'];
   const index = Math.round(degrees / 22.5) % 16;
-  return directions[index;
+  return directions[index];
 }
 
 /**
